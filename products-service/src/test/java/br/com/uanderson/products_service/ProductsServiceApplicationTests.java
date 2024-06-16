52e83094529ad0a72124840460cb003127439ed6
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2024-06-15 21:06:32 -0300

    :tada:projeto base inicial

---
+++ products-service/src/test/java/br/com/uanderson/products_service/ProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uanderson.products_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
