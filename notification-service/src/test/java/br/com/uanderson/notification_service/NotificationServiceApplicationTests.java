ba06fe731eba7a5ed14e4f97a3fc51b04bc776b5
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2024-07-05 18:37:52 -0300

    Configurando o zookeeper e kafka

---
+++ notification-service/src/test/java/br/com/uanderson/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uanderson.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
